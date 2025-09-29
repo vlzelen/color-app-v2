bfc4068b312a82c495011f1c3fb9f26691f0ef76
vlzelen <vzelenskiy2004@gmail.com>
2025-09-29 15:29:36 -0500

    commit changes

--- see-color-app-github-ready copy/vite.config.ts
+++ see-color-app-github-ready copy/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/color-app-v2/',  // Replace with your actual repo name
   server: {
     port: 3000
   }
